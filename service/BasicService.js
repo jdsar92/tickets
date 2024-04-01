a37db965d387cd83036628213196a167c313ccb6
Julian Enrique Diaz Sarmiento <jediazs@co.ibm.com>
2024-04-01 11:22:39 -0500

    Add index.html

--- service/BasicService.js
+++ service/BasicService.js
@@ -17,34 +17,36 @@ exports.getTicketsPage = function(limit,after,properties,propertiesWithHistory,a
   return new Promise(function(resolve, reject) {
     var examples = {};
     examples['application/json'] = {
-  "paging" : {
-    "next" : {
-      "after" : "NTI1Cg%3D%3D",
-      "link" : "?after=NTI1Cg%3D%3D"
-    }
-  },
-  "results" : [ {
-    "properties" : {
-      "createdate" : "2019-10-30T03:30:17.883Z",
-      "hs_lastmodifieddate" : "2019-12-07T16:50:06.678Z",
-      "hs_pipeline" : "support_pipeline",
-      "hs_pipeline_stage" : "open",
-      "hs_ticket_priority" : "HIGH",
-      "hubspot_owner_id" : "910901",
-      "subject" : "troubleshoot report"
-    }
-  }, {
-    "properties" : {
-      "createdate" : "2019-10-30T03:30:17.883Z",
-      "hs_lastmodifieddate" : "2019-12-07T16:50:06.678Z",
-      "hs_pipeline" : "support_pipeline",
-      "hs_pipeline_stage" : "open",
-      "hs_ticket_priority" : "HIGH",
-      "hubspot_owner_id" : "910901",
-      "subject" : "troubleshoot report"
-    }
-  } ]
-};
+      "paging" : {
+        "next" : {
+          "after" : "NTI1Cg%3D%3D",
+          "link" : "?after=NTI1Cg%3D%3D"
+        }
+      },
+      "results" : [ {
+          "properties" : {
+            "name": "Ticket name 1",
+            "createdate" : "2019-10-30T03:30:17.883Z",
+            "hs_lastmodifieddate" : "2019-12-07T16:50:06.678Z",
+            "hs_pipeline" : "support_pipeline",
+            "hs_pipeline_stage" : "open",
+            "hs_ticket_priority" : "CLOSED",
+            "hubspot_owner_id" : "910901",
+            "subject" : "troubleshoot report"
+          }
+        }, {
+          "properties" : {
+            "name": "Ticket name 2",
+            "createdate" : "2019-10-30T03:30:17.883Z",
+            "hs_lastmodifieddate" : "2019-12-07T16:50:06.678Z",
+            "hs_pipeline" : "support_pipeline",
+            "hs_pipeline_stage" : "open",
+            "hs_ticket_priority" : "HIGH",
+            "hubspot_owner_id" : "910901",
+            "subject" : "troubleshoot report"
+          }
+        } ]
+    };
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {

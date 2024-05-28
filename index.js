39de191daf9f9e93b3874e787ad14a0360f92e24
jdsar92 <41712568+jdsar92@users.noreply.github.com>
2024-05-28 17:56:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,94 @@ var options = {
     },
 };
 
-var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
-var app = expressAppConfig.getApp();
+//var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
+//var app = expressAppConfig.getApp();
+
+const app = express();
+
+app.post('/mltw/v1/workflow/watson', (req, res) => {
+  const usersList = req.body;
+
+  // Save the data of user that was sent by the client
+
+  // Send a response to client that will show that the request was successfull.
+  const result = {
+        	"data": {
+        		"clientName": "Pepito Peres",
+        		"Listas": [
+        			{
+        				"listName": "Amigos"
+        			}
+        		],
+        		"inputName": {
+        			"minLength": 2,
+        			"maxLength": 30
+        		},
+        		"listaDestinos": {
+        			"maxNumDestinos": 10,
+        			"minNumDestinos": 1
+        		},
+        		"nombreValidacion": {
+        			"minLength": 2,
+        			"maxLength": 20
+        		},
+        		"bankId": {
+        			"BankType": [
+        				{
+        					"textType": "Cuentas de terceros",
+        					"id": "CDT",
+        					"productType": {
+        						"texto": "Cuenta de Ahorros",
+        						"abreviacion": "CA"
+        					},
+        					"cuentaValidacion": {
+        						"minLength": 9,
+        						"maxLength": 12
+        					},
+        					"valorValidacion": {
+        						"minLength": 4,
+        						"maxLength": 15
+        					}
+        				},
+        				{
+        					"textType": "Cuentas de terceros",
+        					"id": "CDT",
+        					"productType": {
+        						"texto": "Cuenta Corriente",
+        						"abreviacion": "CC"
+        					},
+        					"cuentaValidacion": {
+        						"minLength": 9,
+        						"maxLength": 12
+        					},
+        					"valorValidacion": {
+        						"minLength": 4,
+        						"maxLength": 15
+        					}
+        				},
+        				{
+        					"textType": "Plata",
+        					"id": "DP",
+        					"productType": {
+        						"texto": "Plata",
+        						"abreviacion": "DP"
+        					},
+        					"cuentaValidacion": {
+        						"minLength": 10,
+        						"maxLength": 10
+        					},
+        					"valorValidacion": {
+        						"minLength": 4,
+        						"maxLength": 15
+        					}
+        				}
+        			]
+        		}
+        	},
+        	"errors": []
+        }
+  res.send(result);
+});
 
 // Initialize the Swagger middleware
 http.createServer(app).listen(serverPort, function () {
